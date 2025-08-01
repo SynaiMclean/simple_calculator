575ae1cbf210f2057091c178e73d6b9f3f54f29a
Synai <41596900+SynaiMclean@users.noreply.github.com>
2025-08-01 01:02:44 -0400

    Updated functionality to replace the eval to new built function

--- index.js
+++ index.js
@@ -1,34 +1,64 @@
-let display = document.getElementById('display');
-let operant = document.getElementById('operant');
+let display = document.getElementById("display");
+let operant = document.getElementsByClassName("operant");
 
 function appearInDisplay(value) {
-    display.value += value;
-    
+  display.value += value;
 }
 
-function clearDisplay(){
-    display.value = "";
+function clearDisplay() {
+  display.value = "";
 }
 
-function negativeNum(){
-    display.value *= -1;
+function negativeNum() {
+  display.value *= -1;
 }
-function makeDecimal(){
-    display.value *= 0.01
-    
+function makeDecimal() {
+  display.value *= 0.01;
 }
 
-    
-
-function calculate(){
+function calculate() {
     try{
-        if()
-            
-    }
-    catch(error){
-        display.value = "Error"
+        let operator;
+        let expression = display.value;
+        let arr;
 
-    }
-    
+        if (expression.includes("+")){
+            operator = "+"
+           arr =  expression.split("+")
+        }
+        else if(expression.includes("-")){
+            operator = "-"
+            arr =  expression.split("-")
+        }
+        else if(expression.includes('*')){
+            operator = "*"
+            arr =  expression.split("*")
+        }
+        else if (expression.includes('/')){
+            operator = "/"
+            arr =  expression.split("/")
+        }
+
+        switch (operator) {
+        case "+":
+            display.value = parseFloat(arr[0]) + parseFloat(arr[1]);
+            break;
+        case "-":
+            display.value = parseFloat(arr[0]) - parseFloat(arr[1]);
+             break;
+        case "*":
+            display.value = parseFloat(arr[0]) * parseFloat(arr[1]);
+            break;
+        case "/":
+            display.value = parseFloat(arr[0]) / parseFloat(arr[1]);
+            break;
+            default:
+                display.value = "Try again"
+            break;
+  }
 }
 
+   catch (error) {
+    display.value = "Error";
+  }
+}
